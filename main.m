8a603c5999d32457fac3da79326aafa70da307c0
billdavidzyb <bill-david@163.com>
2016-07-06 22:11:16 +0800

    add dimensions without extra measurement

--- main.m
+++ main.m
@@ -1,5 +1,6 @@
 %% zero and tcp calibration
 % establish robot model
+clear;clc;
 sample = 10;
 
 L12z = 380;
@@ -40,24 +41,23 @@ L_n(2) = Link('d', d_n(2), 'a', a_n(2), 'alpha', p_n(2));
 L_n(3) = Link('d', d_n(3), 'a', a_n(3), 'alpha', p_n(3));
 L_n(4) = Link('d', d_n(4), 'a', a_n(4), 'alpha', p_n(4));
 L_n(5) = Link('d', d_n(5), 'a', a_n(5), 'alpha', p_n(5));
-L_n(6) = Link('d', d_n(6), 'a', a_n(6), 'alpha', p_n(6));
-L_n(7) = Link('d', 0.1,    'a', 0,      'alpha', 0     );
-xb06_n = SerialLink(L_n,'name', 'XB06_t', 'manufacturer', 'Rokae');   
+L_n(6) = Link('d', d_n(6) + 0.1, 'a', a_n(6), 'alpha', p_n(6));
+xb06_n = SerialLink(L_n,'name', 'xb06_n', 'manufacturer', 'Rokae');   
 
-coord = zeros(3,3);
-jac_mat = zeros(3,3,7);
-b = zeros(sample,1);
-A1 = zeros(sample,7);
-A = zeros(sample,5);
+coord = zeros(3,4);
+jac_mat = zeros(4,3,6);
+b = zeros(sample*4,1);
+A1 = zeros(sample*4,6);
+A = zeros(sample*4,5);
 % acquire angle data
 [angle] = test_angle(xb06_n,sample);    %sample * 3 * 7
 
 for i = 1:sample
-    for j = 1:3
+    for j = 1:4
         tmp_frm = xb06_n.fkine(angle(i,j,:));
         coord(:,j) = tmp_frm(1:3,4);
         tmp_jac = xb06_n.jacobn(angle(i,j,:));
-        jac_mat(j,1:3,1:7) = tmp_jac(4:6,1:7);  %%%%%%%%%%%%%%%%%%%%%%
+        jac_mat(j,1:3,1:6) = tmp_jac(4:6,1:6);  %%%%%%%%%%%%%%%%%%%%%%
     end
     a1 = coord(1,1) - coord(1,2);
     a2 = coord(1,3) - coord(1,2);
@@ -73,9 +73,52 @@ for i = 1:sample
     F2 = jac_mat(3,3,:) - jac_mat(2,3,:);
     b(i) = - (a1 * a2 + c1 * c2 + e1 * e2);
     A1(i,:) = a1 * B2 + a2 * B1 + c1 * D2 + c2 * D1 + e1 * F2 + e2 * F1;
+
+    a1 = - a1;
+    a2 = coord(1,4) - coord(1,1);
+    c1 = - c1;
+    c2 = coord(2,4) - coord(2,1);
+    e1 = - e1;
+    e2 = coord(3,4) - coord(3,1);
+    B1 = - B1;
+    B2 = jac_mat(4,1,:) - jac_mat(1,1,:);
+    D1 = - D1;
+    D2 = jac_mat(4,2,:) - jac_mat(1,2,:);
+    F1 = - F1;
+    F2 = jac_mat(4,3,:) - jac_mat(1,3,:);
+    b(i+sample) = - (a1 * a2 + c1 * c2 + e1 * e2);
+    A1(i+sample,:) = a1 * B2 + a2 * B1 + c1 * D2 + c2 * D1 + e1 * F2 + e2 * F1;
+    
+    a1 = coord(1,3) - coord(1,4);
+    a2 = - a2;
+    c1 = coord(2,3) - coord(2,4);
+    c2 = - c2;
+    e1 = coord(3,3) - coord(3,4);
+    e2 = - e2;
+    B1 = jac_mat(3,1,:) - jac_mat(4,1,:);
+    B2 = - B2;
+    D1 = jac_mat(3,2,:) - jac_mat(4,2,:);
+    D2 = - D2;
+    F1 = jac_mat(3,3,:) - jac_mat(4,3,:);
+    F2 = - F2;
+    b(i+2*sample) = - (a1 * a2 + c1 * c2 + e1 * e2);
+    A1(i+2*sample,:) = a1 * B2 + a2 * B1 + c1 * D2 + c2 * D1 + e1 * F2 + e2 * F1;
+    
+    a1 = - a1;
+    a2 = coord(1,2) - coord(1,3);
+    c1 = - c1;
+    c2 = coord(2,2) - coord(2,3);
+    e1 = - e1;
+    e2 = coord(3,2) - coord(3,3);
+    B1 = - B1;
+    B2 = jac_mat(2,1,:) - jac_mat(3,1,:);
+    D1 = - D1;
+    D2 = jac_mat(2,2,:) - jac_mat(3,2,:);
+    F1 = - F1;
+    F2 = jac_mat(2,3,:) - jac_mat(3,3,:);
+    b(i+3*sample) = - (a1 * a2 + c1 * c2 + e1 * e2);
+    A1(i+3*sample,:) = a1 * B2 + a2 * B1 + c1 * D2 + c2 * D1 + e1 * F2 + e2 * F1;
 end
 A = A1(:,1:5);
 [Q,R] = qr(A);
 result = pinv(R)*Q'*b
-
-
